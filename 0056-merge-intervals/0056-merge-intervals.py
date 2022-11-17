d386cf3d81ba9df55e2596738c931f3527b6e5f3
Nikita <99041442+niki2805@users.noreply.github.com>
2022-11-17 13:36:50 -0500

    Time: 423 ms (8.42%), Space: 18.8 MB (35.44%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -0,0 +1,27 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        
+        intervals.sort(key = lambda x : x[0])
+        print(intervals)
+        new=[]
+        start,end=intervals[0][0],intervals[0][1]
+        
+        for i in intervals:
+            #print(new)
+            #print("yo",start,end)
+            
+            curstart,curend=i[0],i[1]
+     
+            if curstart <= end:
+                end=max(end,curend)
+            else:
+                new.append([start,end])
+                start=curstart
+                end=curend
+                
+        new.append([start,end])
+        return new
+            
+            
+            
+        
